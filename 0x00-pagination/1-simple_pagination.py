237c95fa8f7ae7b8bcf57d39eaa53b1014c5e663
leone-nyaga <leonenyaga@gmail.com>
2024-07-23 01:11:00 -0700

    pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes
+two integer arguments page and page_size
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple of size two containing a start index and an end index"""
+    return ((page - 1) * page_size, page * page_size)
