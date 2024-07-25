1ae624f61ceab0232ec35b317b1e9f943346a6da
leone-nyaga <leonenyaga@gmail.com>
2024-07-25 13:30:05 -0700

    CACHING

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,37 +1,39 @@
-from base_caching import BaseCaching
+#!/usr/bin/env python3
+"""LRU Caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class LRUCache(BaseCaching):
-    """ LRU caching system """
+    """ Defines a LRU cache system """
 
     def __init__(self):
-        """ Initialize """
+        """ Initializes the cache """
         super().__init__()
-        self.cache_data = {}
-        self.cache_order = []  # List to maintain the order of keys
+        self.keys = []
 
     def put(self, key, item):
-        """ Add an item to the cache """
-        if key is None or item is None:
-            return
-
-        if key in self.cache_data:
-            # Update the item and move it to the end to mark it as recently used
-            self.cache_order.remove(key)
-        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Cache is full, remove the least recently used item (LRU)
-            lru_key = self.cache_order.pop(0)
-            del self.cache_data[lru_key]
-            print("DISCARD: {}".format(lru_key))
-
-        # Add the item to the cache and update the order list
-        self.cache_data[key] = item
-        self.cache_order.append(key)
+        """
+        Assign item value for the key in
+        self.cache_data
+        if neither key nor item is None
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.keys.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last = self.keys.pop(0)
+                print("DISCARD: {}".format(last))
+                del self.cache_data[last]
+            self.keys.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
-        if key is None or key not in self.cache_data:
-            return None
-        # Move the accessed item to the end to mark it as recently used
-        self.cache_order.remove(key)
-        self.cache_order.append(key)
-        return self.cache_data[key]
+        """
+        Return the value linked to key in self.cache_data.
+        Return None if key is None or doesn't exist
+        """
+        if key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+        return self.cache_data.get(key, None)
