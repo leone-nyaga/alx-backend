7ddf1368d4785e3f971e379e196370de56887dc6
leone-nyaga <leonenyaga@gmail.com>
2024-11-05 22:40:49 +0000

    i18n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Babel setup"""
+from flask import Flask
+from flask_babel import Babel
+
+
+#Flask app instance
+app = Flask(__name__)
+
+
+#Instantiate Babel
+babel = Babel
+
+
+class Config():
+    """Configuration app class"""
+    LANGUAGES: list[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = 'en'
+    BABEL_DEFAULT_TIMEZONE: str = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Render a html template"""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
