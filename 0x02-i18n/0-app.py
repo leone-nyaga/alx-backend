e48e5fb418e99d67cac6b5b54105fd9c5cf6106b
leone-nyaga <leonenyaga@gmail.com>
2024-11-05 19:22:41 +0000

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+"""Flask app"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Render a html template"""
     return render_template('index.html')
 
 if __name__ == '__main__':
